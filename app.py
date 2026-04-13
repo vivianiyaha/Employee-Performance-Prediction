253914164020ee3b5295c5c4d48e073a95c423cb
Vivian Iyaha <90201480+vivianiyaha@users.noreply.github.com>
2026-04-13 15:31:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ df['Department'] = df['Department'].map({
     'Finance': 3
 })
 
-df['Manager Feedback'] = df['ManagerFeedback'].map({
+df['ManagerFeedback'] = df['ManagerFeedback'].map({
     'Poor': 0,
     'Average': 1,
     'Good': 2,
